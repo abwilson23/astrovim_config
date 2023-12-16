7f9b6251aa7f1dee3ad833035954b6ef6fc9a719
Andrew Wilson <Andrew.Wilson@oxfordcc.co.uk>
2023-12-16 18:03:37 +0000

    Initial commit of user config.

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,48 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin", opts = { lazy = false } },
+  { import = "astrocommunity.colorscheme.nightfox-nvim", opts = { lazy = false } },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", lazy = false },
+  { import = "astrocommunity.colorscheme.everforest", opts = { lazy = false } },
+  { import = "astrocommunity.colorscheme.vscode-nvim", lazy = false },
+  { import = "astrocommunity.colorscheme.nvim-juliana", opts = { lazy = false } },
+  { import = "astrocommunity.colorscheme.sonokai", opts = { lazy = false } },
+
+  -- Copilot
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
+  -- git stuff
+  { "sindrets/diffview.nvim" },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim", -- optional
+      "ibhagwan/fzf-lua", -- optional
+    },
+    config = function() require("neogit").setup {} end,
+    lazy = false,
+  },
+  -- Lua
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim", opts = {} },
+
+  -- Relevant plugin packs
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.svelte" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,28 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 }
